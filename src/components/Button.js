02a885fca9a39a537b89cc4930dd556736d07f1f
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-22 12:19:24 +0530

    Added buttons

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class Button extends Component {
+    render(){
+        return (
+            <div>
+                <button class="calc-button">1</button>
+            </div>
+        )
+    }
+}
+export default Button;
