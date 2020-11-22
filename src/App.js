551a037a19f6a4f5e2f4289e99c6947819c9c76d
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-22 16:07:55 +0530

    values adding

--- src/App.js
+++ src/App.js
@@ -13,35 +13,37 @@ class App extends Component {
     this.setState({result:'0'})
   }
 
+  addToCurrent = (symbol) => {
+    this.setState({current: this.state.current+symbol});
+  }
+
   render(){
     const buttons = [
-      {symbol:'C', cols:3, action:this.reset},
-      {symbol:'/', cols:1, action:this.addToCurrent},
-      {symbol:'7', cols:1, action:this.addToCurrent},
-      {symbol:'8', cols:1, action:this.addToCurrent},
-      {symbol:'9', cols:1, action:this.addToCurrent},
-      {symbol:'X', cols:1, action:this.addToCurrent},
-      {symbol:'4', cols:1, action:this.addToCurrent},
-      {symbol:'5', cols:1, action:this.addToCurrent},
-      {symbol:'6', cols:1, action:this.addToCurrent},
-      {symbol:'-', cols:1, action:this.addToCurrent},
-      {symbol:'1', cols:1, action:this.addToCurrent},
-      {symbol:'2', cols:1, action:this.addToCurrent},
-      {symbol:'3', cols:1, action:this.addToCurrent},
-      {symbol:'+', cols:1, action:this.addToCurrent},
-      {symbol:'0', cols:2, action:this.addToCurrent},
-      {symbol:'.', cols:1, action:this.addToCurrent},
-      {symbol:'=', cols:1, action:this.addToCurrent},
+      {symbol: 'C', cols: 3, action:this.reset},
+      {symbol: '/', cols: 1, action:this.addToCurrent},
+      {symbol: '7', cols: 1, action:this.addToCurrent},
+      {symbol: '8', cols: 1, action:this.addToCurrent},
+      {symbol: '9', cols: 1, action:this.addToCurrent},
+      {symbol: 'X', cols: 1, action:this.addToCurrent},
+      {symbol: '4', cols: 1, action:this.addToCurrent},
+      {symbol: '5', cols: 1, action:this.addToCurrent},
+      {symbol: '6', cols: 1, action:this.addToCurrent},
+      {symbol: '-', cols: 1, action:this.addToCurrent},
+      {symbol: '1', cols: 1, action:this.addToCurrent},
+      {symbol: '2', cols: 1, action:this.addToCurrent},
+      {symbol: '3', cols: 1, action:this.addToCurrent},
+      {symbol: '+', cols: 1, action:this.addToCurrent},
+      {symbol: '0', cols: 2, action:this.addToCurrent},
+      {symbol: '.', cols: 1, action:this.addToCurrent},
+      {symbol: '=', cols: 1, action:this.addToCurrent},
     ];
 
     return (
       <div className="App">
         <input className="result" type="text" value={this.state.current} />
-        {
-          buttons.map((btn,i)=>{
-            return <Button symbol={btn.symbol} cols={btn.cols} action={(symbol)=> btn.action} />
-          })
-        }
+        {buttons.map((btn,i)=> {
+            return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action(symbol)} />
+          })}
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ class Button extends Component {
     render(){
         return (
             <div>
-                <button class="calc-button">1</button>
+                <button className="calc-button" onClick={() => this.props.action(this.props.symbol)}>{this.props.symbol}</button>
             </div>
         )
     }
