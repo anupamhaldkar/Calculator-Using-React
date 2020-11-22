1ab2a873022978436103d045b248a10d35b2c1ae
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-22 17:51:24 +0530

    calculate Logic

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ class App extends Component {
     this.setState({current: this.state.current+symbol});
    }
   }
+
+  calculate = (symbol) => {
+    
+  }
   render(){
     const buttons = [
       {symbol: 'C', cols: 3, action:this.reset},
@@ -46,7 +50,7 @@ class App extends Component {
       {symbol: '+', cols: 1, action:this.addToCurrent},
       {symbol: '0', cols: 2, action:this.addToCurrent},
       {symbol: '.', cols: 1, action:this.addToCurrent},
-      {symbol: '=', cols: 1, action:this.addToCurrent},
+      {symbol: '=', cols: 1, action:this.calculate},
     ];
 
     return (
