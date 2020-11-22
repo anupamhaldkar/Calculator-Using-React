e127833a672322c8ad1f4ea133246495c8da58c0
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-22 18:08:57 +0530

    Completed logic

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   }
 
   addToCurrent = (symbol) => {
-    if(['/','X','+','-'].indexOf(symbol)>-1){
+    if(['/','*','+','-'].indexOf(symbol)>-1){
       let {previous} = this.state;
       previous.push(this.state.current+" "+symbol);
       this.setState({previous,nextIsReset:true});
@@ -30,7 +30,11 @@ class App extends Component {
   }
 
   calculate = (symbol) => {
-    
+    let {current,previous,nextIsReset} = this.state;
+    if(previous.length>0&&this.state.current!='.'){
+      current = eval(String(previous[previous.length-1]+current));
+      this.setState({current, previous:[], nextIsReset:true})
+    }
   }
   render(){
     const buttons = [
@@ -39,7 +43,7 @@ class App extends Component {
       {symbol: '7', cols: 1, action:this.addToCurrent},
       {symbol: '8', cols: 1, action:this.addToCurrent},
       {symbol: '9', cols: 1, action:this.addToCurrent},
-      {symbol: 'X', cols: 1, action:this.addToCurrent},
+      {symbol: '*', cols: 1, action:this.addToCurrent},
       {symbol: '4', cols: 1, action:this.addToCurrent},
       {symbol: '5', cols: 1, action:this.addToCurrent},
       {symbol: '6', cols: 1, action:this.addToCurrent},
