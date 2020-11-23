5aa5cc79b502d37ce91a4be5b61c511aedebae73
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-23 17:46:17 +0530

    Css + Logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react'
 import Button from './components/Button'
+import "./css/style.css"
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       current : '0',
-      previous:[]
+      previous:[],
+      nextIsReset: false
     }
   }
 
   reset = () => {
-    this.setState({result:'0'})
+    this.setState({current:'0',previous:[],nextIsReset:false})
   }
 
   addToCurrent = (symbol) => {
+    if(['/','X','+','-'].indexOf(symbol)>-1){
+      let {previous} = this.state;
+      previous.push(this.state.current+" "+symbol);
+      this.setState({previous,nextIsReset:true});
+    }
+    else if((this.state.current==='0'&& symbol!='.')||this.state.nextIsReset){
+      this.setState({current:symbol,nextIsReset:false});
+    }
+    else {
     this.setState({current: this.state.current+symbol});
+   }
   }
-
   render(){
     const buttons = [
       {symbol: 'C', cols: 3, action:this.reset},
@@ -40,7 +51,13 @@ class App extends Component {
 
     return (
       <div className="App">
+        {
+          this.state.previous.length>0?
+          <div className="floaty-last">{this.state.previous[this.state.previous.length-1]}</div>
+          :null
+        }
         <input className="result" type="text" value={this.state.current} />
+        <br/>
         {buttons.map((btn,i)=> {
             return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action(symbol)} />
           })}
