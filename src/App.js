d87a817d22e82abde94533a85e4980353af21286
Anupam Haldkar <ahdev2020@outlook.com>
2020-11-25 18:09:58 +0530

    Fixed Typo �

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
       {symbol: '+', cols: 1, action:this.addToCurrent},
       {symbol: '0', cols: 2, action:this.addToCurrent},
       {symbol: '.', cols: 1, action:this.addToCurrent},
-      {symbol: '=', cols: 1, action:this.calculate},
+      {symbol: '=', cols: 1, action:this.calculate}
     ];
 
     return (
